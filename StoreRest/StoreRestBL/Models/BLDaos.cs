240c41aeb933385a01dd4f1d1cfd8f425cf7612b
daniel <danielsmcelhaney@gmail.com>
2016-10-10 10:42:00 -0400

    Add service reference and Daos, mapper

---
+++ StoreRest/StoreRestBL/Models/BLDaos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreRestBL.Models
+{
+  public class productDao
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool Discount { get; set; }
+
+  }
+
+  public class orderDao
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<productDao> products { get; set; }
+    public decimal totalPrice { get; set; }
+
+  }
+}
---
+++ StoreRest/StoreRestBL/Models/BLMapper.cs
@@ -0,0 +1,67 @@
+﻿using StoreRestBL.StoreDASoap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreRestBL.Models
+{
+  class BLMapper
+  {
+
+    public product ProductDaoToProduct(productDao pd)
+    {
+      product p = new product();
+      p.Id = pd.Id;
+      p.Name = pd.Name;
+      p.Price = pd.Price;
+      p.Discount = pd.Discount;
+      return p;
+    }
+
+    public productDao ProductToProductDao(product p)
+    {
+      productDao pd = new productDao();
+      pd.Id = p.Id;
+      pd.Name = p.Name;
+      pd.Price = p.Price;
+      pd.Discount = p.Discount;
+      return pd;
+    }
+
+    public order OrderDaoToOrder(orderDao od)
+    {
+      order o = new order();
+      o.Id = od.Id;
+      o.Name = od.Name;
+      o.totalPrice = od.totalPrice;
+      List<product> lp = new List<product>();
+      foreach (var item in od.products)
+      {
+        lp.Add(ProductDaoToProduct(item));
+      }
+      o.products = lp.ToArray();
+      return o;
+    }
+
+    public orderDao OrderToOrderDao(order o)
+    {
+      orderDao od = new orderDao();
+      od.Id = o.Id;
+      od.Name = o.Name;
+      od.products = new List<productDao>();
+      foreach (var item in o.products)
+      {
+        od.products.Add(ProductToProductDao(item));
+      }
+      od.totalPrice = o.totalPrice;
+      return od;
+    }
+
+  }
+}
+
+
+  }
+}
