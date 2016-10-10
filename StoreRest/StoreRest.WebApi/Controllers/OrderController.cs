1557b2a2925f53c95ea9df0db30daf323cc8edbe
daniel <danielsmcelhaney@gmail.com>
2016-10-10 12:48:39 -0400

    add product and order controllers to store web api

---
+++ StoreRest/StoreRest.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using StoreRest.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoreRest.WebApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+    private readonly StoreWebApiBLHelper rblh = new StoreWebApiBLHelper();
+
+    [HttpGet]
+    public HttpResponseMessage Get()
+    {
+      return Request.CreateResponse(HttpStatusCode.OK, rblh.GetOrders());
+    }
+
+    [HttpPost]
+    public HttpResponseMessage Post(orderDto o)
+    {
+      if (rblh.AddOrder(o))
+      {
+        return Request.CreateResponse(HttpStatusCode.OK);
+      }
+      else
+      {
+        return Request.CreateResponse(HttpStatusCode.InternalServerError);
+      }
+    }
+
+    [HttpDelete]
+    public HttpResponseMessage Delete(orderDto o)
+    {
+      if (rblh.DeleteOrder(o))
+      {
+        return Request.CreateResponse(HttpStatusCode.OK);
+      }
+      else
+      {
+        return Request.CreateResponse(HttpStatusCode.InternalServerError);
+      }
+    }
+
+  }
+}
+
---
+++ StoreRest/StoreRest.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using StoreRest.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoreRest.WebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+    private readonly StoreWebApiBLHelper rblh = new StoreWebApiBLHelper();
+
+    [HttpGet]
+    public HttpResponseMessage Get()
+    {
+      List<productDto> lpd = rblh.GetProducts();
+      return Request.CreateResponse(HttpStatusCode.OK, lpd);
+    }
+
+    [HttpPost]
+    public HttpResponseMessage Post(productDto p)
+    {
+      if (rblh.AddProduct(p))
+      {
+        return Request.CreateResponse(HttpStatusCode.OK);
+      }
+      else
+      {
+        return Request.CreateResponse(HttpStatusCode.InternalServerError);
+      }
+    }
+
+    [HttpDelete]
+    public HttpResponseMessage Delete(productDto p)
+    {
+      if (rblh.DeleteProduct(p))
+      {
+        return Request.CreateResponse(HttpStatusCode.OK);
+      }
+      else
+      {
+        return Request.CreateResponse(HttpStatusCode.InternalServerError);
+      }
+    }
+
+  }
+}
+
