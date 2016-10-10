c63af541f91257e41c65e59e7a2d8fdf761f4a8d
daniel <danielsmcelhaney@gmail.com>
2016-10-10 17:08:50 -0400

    Get products in productController index view

--- StoreClient/StoreClient/Controllers/ProductController.cs
+++ StoreClient/StoreClient/Controllers/ProductController.cs
@@ -1,17 +1,55 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using StoreClient.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace StoreClient.Controllers
 {
     public class ProductController : Controller
     {
         // GET: Product
-        public ActionResult Index()
+        public async System.Threading.Tasks.Task<ActionResult> Index()
         {
-            return View();
+          string url = "";
+          try
+          {
+           url = "http://localhost/StoreRest/api/product";
+            using (HttpClient client = new HttpClient())
+            {
+              client.BaseAddress = new Uri(url);
+              client.DefaultRequestHeaders.Accept.Clear();
+              //client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+              System.Net.Http.HttpResponseMessage response = await client.GetAsync(url);
+               if (response.IsSuccessStatusCode)
+               {
+                 var p = await response.Content.ReadAsStringAsync();
+                 //convert List of Snippets into objects
+               ProductListModel s = new ProductListModel();
+                s.lpm = JsonConvert.DeserializeObject<List<ProductModel>>(p);
+                ViewBag.Results = s.lpm.Count.ToString();
+                return View(s.lpm);
+               }
+               else
+              {
+                ViewBag.Results = "0";
+               return View(new List<ProductModel>());
+              }
+            } //end using System.Net.Http.HttpClient client
+         }
+           catch (Exception)
+         {
+           ViewBag.Results = "0";
+           return View(new List<ProductModel>());
         }
-    }
-}
+        } //end public async Results method
+       
+  }
+ }
---
+++ StoreClient/StoreClient/Models/ClientModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreClient.Models
+{
+  public class ProductListModel
+  {
+    public List<ProductModel> lpm { get; set; }
+  }
+
+  public class ProductModel
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool Discount { get; set; }
+
+  }
+
+  public class OrderListModel
+  {
+    public List<OrderModel> lom { get; set; }
+  }
+
+  public class OrderModel
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<ProductModel> products { get; set; }
+    public decimal totalPrice { get; set; }
+  }
+
+}
